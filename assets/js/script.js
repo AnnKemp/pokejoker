e5bf04f8a510470d9087fd8256d3e7fdb6be33bc
AnnKemp <ann.kemp@scarlet.be>
2020-01-27 17:02:31 +0100

    new fetch for the image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,62 @@
-//document.getElementById('inputBtn').addEventListener('click', function getName(){
+(() => {
+    // variabelen initialiseren
+    let ImageIndex;
 
+    document.getElementsByTagName("body")[0].addEventListener("load", getPokemon);
 //fetch JSON
-async function getPokemon(name) {
+async function getPokemon() {
 
-//empty moves array every time you search for new pokemon
-   // array.length = 0;
-//if input is empty alert the user to enter a pokemon name
-    //   if(name === ""){
-    //       alert('type in a pokemon name!')
-    //  }
 //fetch stream of data
-    const pokemonPlain = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-    const pokemonSpecies = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${name}`);
+    const pokemonPlain = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151`);
 
 //convert to json
     const pObject = await pokemonPlain.json();
-    const pSpecies = await pokemonSpecies.json();
-
     console.log(pObject);
-    console.log(pSpecies);
 
+    let randomSprite = [];
+
+// to random select 4 images by id
     for (i = 1; i <= 4; i++) {
-
-        let randomSprite = [];
         randomSprite[i] = Math.floor(Math.random() * 151) + 1; // returns a random integer from 1 to 151
-    }
-let ImageIndex = randomSprite[Math.floor(Math.random() * 5)]; // to select the image to show
-        //ImageIndex.......sprites.front_default;
-
-   /* function same(randomSprite[i]) {
-        return randomSprite[i] !== ImageValue;
+      console.log( randomSprite[i] );
     }
 
-    function myFunction() {
-        document.getElementsByClassName("namePoke");innerHTML= randomSprite.filter(same);
-    }*/
+    /*  for (i = 0; i < 4; i++) {
+          document.querySelector(".guessButton")[i].innerHTML ="image_"+i.name;
+      }*/
+    let randomGetal = Math.floor(Math.random() * 5);
+    console.log(randomGetal);
+    ImageIndex = randomSprite[randomGetal]; // to select the image to show
+    console.log(ImageIndex);
+    return ImageIndex;
 }
-getPokemon(151);
+
+    console.log(ImageIndex);
+
+  /*  async function getPokemonImage(name) {
+        //fetch stream of data
+        const pokemonImage = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+        const pImage = await pokemonImage.json();
+        console.log(pImage);
+    }
+
+    getPokemonImage(ImageIndex);
+    //ImageIndex.
+   // PokemonIcon
+   // pObject.sprites.front_default;
+
+    /* function same(randomSprite[i]) {
+     return randomSprite[i] !== ImageValue;
+ }
+ function myFunction() {
+     document.getElementsByClassName("namePoke");innerHTML= randomSprite.filter(same);
+ }*/
+
+    // voor de buttons
+    //   document.querySelector(".guessButton")[i].innerHTML = item[i].name;
 
 
-
-
-
+getPokemon();
 
 //});
+})();
