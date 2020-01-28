c423440cedcc1cc436f0475216360ca719a44d94
AnnKemp <ann.kemp@scarlet.be>
2020-01-28 11:56:23 +0100

    adding image

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,14 +13,13 @@ async function getPokemon() {
     const pObject = await pokemonPlain.json();
    console.log(pObject); // testing in the console
 
-    console.log(pObject.results[0].name);
     // generate 4 random numbers to use as random id's to select 4 random pokemons
     for (let i = 1; i <= 4; i++) {
         randomSprite[i] = Math.floor(Math.random() * 151) + 1;     // returns a random integer from 1 to 151
         // console.log(randomSprite[i] ); // this works fine
     }
 
-    // select buttons to display 4 random pokemon names
+    // select buttons to display 4 random pokemon names   ---this works!
     for (let y = 0; y < 4; y++) {
         document.querySelectorAll(".guessButton")[y].innerHTML= pObject.results[randomSprite[y+1]].name;
     }
@@ -29,25 +28,24 @@ async function getPokemon() {
 
 getPokemon(); // call the function
 
-  //console.log(pObject); // testing in the console
-
+  //console.log(pObject); // testing in the console dus met ne return krijg ik de pObject data niet uit de functie ???!!! why?!!!
 
     // to generate a random number between 0 and 4 to select the random id of the image we are going to show
     let randomGetal = Math.floor(Math.random() * 5); // this works fine!
-    // console.log(randomGetal);
 
-    ImageIndex = randomSprite[randomGetal]; // to select the image to show
-    console.log(ImageIndex);  // this werk niet
-    //return ImageIndex;
+   // ImageIndex = randomSprite[randomGetal]; // to select the random image to show from the four
 
-  /*  async function getPokemonImage(name) {
+  async function getPokemonImage(name) {
         //fetch stream of data
         const pokemonImage = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
         const pImage = await pokemonImage.json();
-        console.log(pImage);
+      //  console.log(pImage.sprites.front_default);  // this works
+      document.querySelector(".PokemonIcon").src=pImage.sprites.front_default;    // this works!
     }
 
-    getPokemonImage(ImageIndex);
+    getPokemonImage(55);
+
+
     //ImageIndex.
    // PokemonIcon
    // pObject.sprites.front_default;
@@ -55,15 +53,11 @@ getPokemon(); // call the function
     /* function same(randomSprite[i]) {
      return randomSprite[i] !== ImageValue;
  }
- function myFunction() {
-     document.getElementsByClassName("namePoke");innerHTML= randomSprite.filter(same);
- }*/
-
-    // voor de buttons
-    //   document.querySelector(".guessButton")[i].innerHTML = item[i].name;
 
 
 
+
+*/
 
 //});
 })();
