acd1248640185b9f27ed93f188d8475d0313b8be
AnnKemp <ann.kemp@scarlet.be>
2020-01-28 14:35:41 +0100

    pokemon pictures and names display without errors now

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,30 +11,29 @@ async function getPokemon() {
 
 //convert to json
     const pObject = await pokemonPlain.json();
-   console.log(pObject); // testing in the console
+    console.log(pObject); // testing in the console
 
     // generate 4 random numbers to use as random id's to select 4 random pokemons
     for (let i = 1; i <= 4; i++) {
         randomSprite[i] = Math.floor(Math.random() * 151) + 1;     // returns a random integer from 1 to 151
-        // console.log(randomSprite[i] ); // this works fine
+        console.log(randomSprite[i]); // this works fine
     }
 
     // select buttons to display 4 random pokemon names   ---this works!
     for (let y = 0; y < 4; y++) {
-        document.querySelectorAll(".guessButton")[y].innerHTML= pObject.results[randomSprite[y+1]].name;
+        document.querySelectorAll(".guessButton")[y].innerHTML = pObject.results[randomSprite[y + 1]].name;
     }
 
     // to generate a random number between 0 and 4 to select the random id of the image we are going to show
     let randomGetal = Math.floor(Math.random() * 5); // this works fine!
+    if (randomGetal==0){
+        randomGetal=1;
+    }
 
     ImageIndex = randomSprite[randomGetal]; // to select the random image to show from the four selected pokemons
-
+    ImageIndex=ImageIndex+1;
     getPokemonImage(ImageIndex);
-    console.log(ImageIndex); // to test which numbers don't give an image and to solve this problem
-
-    //return pObject; // release the data
 }
-
 getPokemon(); // call the function
 
   //console.log(pObject); // testing in the console dus met ne return krijg ik de pObject data niet uit de functie ???!!! why?!!!
@@ -42,29 +41,23 @@ getPokemon(); // call the function
         //fetch stream of data
         const pokemonImage = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
         const pImage = await pokemonImage.json();
-        //  console.log(pImage.sprites.front_default);  // this works
+
+     console.log(pImage.sprites.front_default);  // this works
+        console.log(pImage.name);
+        console.log(pImage.id);
         document.querySelector(".PokemonIcon").src=pImage.sprites.front_default;    // this works!
     }
 
-
-
-
-
-
-
-
-    //ImageIndex.
-   // PokemonIcon
-   // pObject.sprites.front_default;
-
-    /* function same(randomSprite[i]) {
+    /*
+    // het vergelijken of er twee van die randomwaarde hetzelfde zijn heb ik nog niet gedaan
+    function same(randomSprite[i]) {
      return randomSprite[i] !== ImageValue;
+
+     //na die eerste fetch de data in een array steken zodat die constant bereikbaar zijn heb ik nog niet gedaan
+     // zo ook voor de tweede fetch
+
+     // een catch - error constructie voorzien, dat heb ik ook nog niet gedaan
  }
-
-
-
-
 */
 
-//});
 })();
