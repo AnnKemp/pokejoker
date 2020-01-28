e2071a837e2f8e8da2da99f4e701fcdb8d45a3b0
moeti <moetichentuf@hotmail.com>
2020-01-28 11:24:21 +0100

    added typewriter effect to insults

--- assets/js/insult.js
+++ assets/js/insult.js
@@ -1,9 +1,32 @@
 
+
 async function insults(lala) {
     //fetch stream of data
     const pokemonImage = await fetch(`https://amused.api.stdlib.com/insult@1.0.0/`);
     const pImage = await pokemonImage.json();
-    console.log(pImage);
+
     document.querySelector("p.insultText").innerHTML =pImage;
+
+    var str = document.querySelector("p.insultText").innerHTML,
+        i = 0,
+        isTag,
+        text;
+
+    (function type() {
+        text = str.slice(0, ++i);
+        if (text === str) return;
+
+        document.querySelector("p.insultText").innerHTML = text;
+
+        var char = text.slice(-1);
+        if( char === '<' ) isTag = true;
+        if( char === '>' ) isTag = false;
+
+        if (isTag) return type();
+        setTimeout(type, 100);
+    }());
+
+
 }
+
 insults();
