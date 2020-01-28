33b3fa691fd43daf23a435a20539d82f5076a95f
moeti <moetichentuf@hotmail.com>
2020-01-28 12:28:04 +0100

    added typewriter to more text

--- assets/js/insult.js
+++ assets/js/insult.js
@@ -5,8 +5,26 @@ async function insults(lala) {
     const pokemonImage = await fetch(`https://amused.api.stdlib.com/insult@1.0.0/`);
     const pImage = await pokemonImage.json();
 
-    document.querySelector("p.insultText").innerHTML =pImage;
+    document.querySelector("p.text").innerHTML =pImage;
 
+    var str = document.querySelector("p.text").innerHTML,
+        i = 0,
+        isTag,
+        text;
+
+    (function type() {
+        text = str.slice(0, ++i);
+        if (text === str) return;
+
+        document.querySelector("p.text").innerHTML = text;
+
+        var char = text.slice(-1);
+        if( char === '<' ) isTag = true;
+        if( char === '>' ) isTag = false;
+
+        if (isTag) return type();
+        setTimeout(type, 100);
+    }());
 
 
 }
