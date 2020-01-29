021c459f57e787c8fde1edd9d37e6c0e9d7dfab5
AnnKemp <ann.kemp@scarlet.be>
2020-01-29 14:57:57 +0100

    it works! And I still have to fix a sentence

--- assets/js/func.js
+++ assets/js/func.js
@@ -3,6 +3,7 @@
     let Name;
 
   for(let i=0;i<4 ;i++) {
+      document.getElementById("lala").innerHTML="There is a wild pokemon rapidly coming towards you, wich one is it?";
 
       document.getElementsByClassName("guessButton")[i].addEventListener("click", function(){
 
@@ -18,12 +19,18 @@
           // Retrieve data from session
           let ImageIndexName= sessionStorage.getItem("nameImage");
 
+
           //console.log(ImageIndexName);
 
         if(Name == ImageIndexName) {
-            document.querySelector(".guessPokemon").style.display = none;
-            document.querySelector(".catchIt").style.display = block;
-       }
+
+            document.getElementById("lala").innerHTML=" ";
+
+            document.querySelector(".guessPokemon").style.display="none";
+            document.querySelector(".catchIt").style.display="block";
+       }else{
+            document.getElementById("lala").innerHTML="you missed!";
+        }
       }
 
 
