204ec672355e5c214dffdd541156aa04ad44b209
AnnKemp <ann.kemp@scarlet.be>
2020-01-29 10:42:06 +0100

    alert on buttons on click

---
+++ assets/js/func.js
@@ -0,0 +1,42 @@
+(() => {
+
+document.getElementById("pokeGuess1").addEventListener("click", checkFunction);
+document.getElementById("pokeGuess2").addEventListener("click", checkFunction);
+document.getElementById("pokeGuess3").addEventListener("click", checkFunction);
+document.getElementById("pokeGuess4").addEventListener("click", checkFunction);
+
+function checkFunction() {
+     alert("clicked");
+    /*
+    let button_1=document.getElementById("pokeGuess1").innerHTML;
+    let button_2=document.getElementById("pokeGuess1").innerHTML;
+    let button_3=document.getElementById("pokeGuess1").innerHTML;
+    let button_4=document.getElementById("pokeGuess1").innerHTML;
+
+  if(idName == ImageIndex.name){
+      document.querySelector(".guessPokemon").style.display=none;
+      document.querySelector(".catchIt").style.display=block; */
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
