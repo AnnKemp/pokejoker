9539c7f6cbc1db715e6305d9c45123b2f2a2899d
AnnKemp <ann.kemp@scarlet.be>
2020-02-01 14:47:19 +0100

    add rotating and shrinking hit pokemon

--- assets/js/gameScript_page.js
+++ assets/js/gameScript_page.js
@@ -6,15 +6,27 @@ function myMove() {  // move pokebal or candy or rock
     document.getElementById("myAnimation").style.display="block";
     function frame() {
         if (pos == 16) {
+            // remove interval
             clearInterval(id);
-            document.getElementById("secondPicture").style.display="none";
 
-// document.getElementById("secondPicture").rotate(20 * Math.PI / 180);
+            // make pokemon dissapear
+            //wordt zoiets
+           // document.getElementById("secondPicture").style.transition = "all 2s";
+
+            // call 'die' in the css and start the animation
+            document.getElementById("secondPicture").style.animation = "die 2s ease-in";
+            document.getElementById("secondPicture").style.WebkitAnimation = "die 2s ease-in"; // Code for Safari 4.0 - 8.0
+
+            //document.getElementById("secondPicture").rotate(20 * Math.PI / 180);
+
+            //animation of the ball or candy or rock
+
             document.getElementById('myAnimation').style.WebkitTransitionDuration="1s";
             document.getElementById('myAnimation').style.webkitTransform = 'rotate(40deg)';
 // elem.setAttribute('style','bottom:'+(pos)+'px;');
 
         } else {
+            // bal moves to the right
             pos++;
             elem.style.left = pos + '5px';
 
@@ -24,9 +36,10 @@ function myMove() {  // move pokebal or candy or rock
 
 document.getElementById("myAnimation").style.display="none";
 
-//#myAnimation    background-image: url("../img/used/pokeball_pokeball.svg");
 
-// voor die prentjes voor het werp-voorwerp te vervangen
+
+// onclick on word/name change throw-item into bal or candy or rock and start function myMove
+
 document.getElementById("pokeball").addEventListener("click", function(){
 
     document.getElementById("myAnimation").style.backgroundImage= "url('assets/img/used/pokeball_pokeball.svg')";
@@ -38,6 +51,6 @@ document.getElementById("rock").addEventListener("click", function(){
 });
 document.getElementById("candy").addEventListener("click", function(){
 
-    document.getElementById("myAnimation").style.backgroundImage=" url('assets/img/used/candy_candy.svg')";
+    document.getElementById("myAnimation").style.backgroundImage="url('assets/img/used/candy_candy.svg')";
     myMove();
 });
